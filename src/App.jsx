967c2b9a8c90acb06bb6cd428d4e30afbe5f7178
Toby Ceballos <96705059+TobyCeballos@users.noreply.github.com>
2023-06-21 22:02:57 -0300

    corrección de error

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 
 import ItemListContainer from './components/ItemListContainer'
-import NavBar from './components/NavBar'
 import MyRoutes from './routers/routes'
 
 function App() {
   return (
     <>
       <main className='w-full h-screen bg-slate-300'>
-        <NavBar/>
         <MyRoutes/>
       </main>
     </>
